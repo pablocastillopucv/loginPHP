e55d72b4cfc0808521b136e167c97d65ef6620e7
Pablete <pablo.castillo.p@mail.pucv.cl>
2020-09-04 18:47:46 -0400

    style(registrate.view): mejora estilo de la vista

--- views/registrate.view.php
+++ views/registrate.view.php
@@ -4,13 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrate</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
     <div class="contenedor">
         <h1 class="titulo">Registrate</h1>
         <hr class="border">
-       
-    </div>
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="formulario" name="login">
+            <div class="form-group">
+                <i class="icono izquierda fa fa-user"></i><input type="text" name="usuario" class="usuario" placeholder="Usuario">
+            </div>
+            <div class="form-group">
+                <i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password" placeholder="Contraseña">
+            </div>
+            <div class="form-group">
+                <i class="icono izquierda fa fa-lock"></i><input type="password" name="password2" class="password_btn" placeholder="Repetir Contraseña">
+                <i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
+            </div>
+        </form>
+        <p class="texto-registrate">
+           Ya tienes Cuenta ?
+            <a href="login.php">Iniciar Sesion</a>
+        </p>
 </body>
 </html>
